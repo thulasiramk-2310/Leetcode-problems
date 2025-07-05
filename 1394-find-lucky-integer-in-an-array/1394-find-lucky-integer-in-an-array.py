4d7fcb1515f5bf74128a96ea29a5799c2ad77e83
thulasiramk-2310 <thulasiramk.aiml2024@citchennai.net>
2025-07-05 14:16:58 +0530

    Time: 0 ms (100%), Space: 17.7 MB (77.76%) - LeetHub

---
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLucky(self, arr: list[int]) -> int:
+        freq = Counter(arr)
+        lucky = -1
+
+        for num, count in freq.items():
+            if num == count:
+                lucky = max(lucky, num)
+
+        return lucky
