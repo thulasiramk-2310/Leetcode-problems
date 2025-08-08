5d14e263ab72372f1384c302b2c35a01ee9cb1ad
thulasiramk-2310 <thulasiramk.aiml2024@citchennai.net>
2025-08-08 23:50:41 +0530

    Time: 0 ms (100%), Space: 44.8 MB (79.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,18 @@
 class Solution {
 
-public int[] twoSum(int[] nums, int target){
+    public int[] twoSum(int[] nums, int target) {
 
-for(int i=1;i < nums.length;i++){
-for(int j=i;j<nums.length;j++){
-if(nums[j] + nums[j-i] == target){
-return new int[]{j , j-i};
-}
-}
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[] { j, j - i };
+                }
+            }
 
-}
+        }
 
-return null;
+        return null;
 
-
-}
-
+    }
 
 }
