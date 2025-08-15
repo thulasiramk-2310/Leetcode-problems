d2401e79d394f5268713c54a735806d2f352fb9e
thulasiramk-2310 <thulasiramk.aiml2024@citchennai.net>
2025-08-15 21:10:47 +0530

    Time: 0 ms (100%), Space: 12.1 MB (100%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 0 or n==0:
+            return False
+
+        while n % 4 == 0 :
+            n //= 4 
+        return n==1   
